ef90a4a3b3c2fe875e23d72b6d0b16eeadba57f5
Şevval Özdamar <ozdamarsevval.01@gmail.com>
2023-06-04 22:26:16 +0300

    done.

--- app/src/main/java/com/ozdamarsevval/carsystemapp/module/FirebaseModule.kt
+++ app/src/main/java/com/ozdamarsevval/carsystemapp/module/FirebaseModule.kt
@@ -1,7 +1,6 @@
 package com.ozdamarsevval.carsystemapp.module
 
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
